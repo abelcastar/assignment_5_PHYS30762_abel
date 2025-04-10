4481a5848e8401a4de884daa1ededc6540c2877e
abelcastar <acastar06@gmail.com>
2025-04-10 12:44:27 +0100

    made all files required and created a new repository

---
+++ assignment-5.cpp
@@ -0,0 +1,35 @@
+// PHYS 30762 Programming in C++
+// Assignment 5
+// Practice inheritance in C++ classes
+// See slides for guidance
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<cmath>
+
+using std::string;
+
+// Beginning of nucleus class 
+class Nucleus
+{
+private:
+  //...
+public:
+  //...
+protected: 
+  //...
+
+};
+// End of nucleus class and associated member functions
+
+// Derived classes for radioactive and stable nuclei, photon and electron classes
+
+// Note: implementation of functions from a class should be done outside the class in the cpp file
+
+// Main program
+int main()
+{
+  //...
+  return 0;
+}
